ae0d11cd808fb2ab348ba911fdcc6e262b9e19fd
Mathias Roggeman <mathias.roggeman@hotmail.be>
2019-03-25 16:19:15 +0100

    fixed date to be correct with localzone

--- brexitTimer/views.py
+++ brexitTimer/views.py
@@ -6,8 +6,8 @@ from django import template
 from django.utils.timesince import timeuntil
 
 now = datetime.now()
-brexitDate  = datetime(2019, 3, 29, 12 , 00, 00, 0000)
-d = (brexitDate - now)
+brexitDateLocalTime  = datetime(2019, 3, 30, 0 )
+d = (brexitDateLocalTime  - now)
 Timeuntil = str(d).split(".")[0]
 #timeUntil = timeuntil(brexitDate)
 # Create your views here.
