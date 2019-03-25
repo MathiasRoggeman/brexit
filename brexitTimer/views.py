68ff75a7e367c5a145065190584e54a18973525d
Mathias Roggeman <mathias.roggeman@hotmail.be>
2019-03-25 14:53:35 +0100

    add Timeuntil function in indexview

--- brexitTimer/views.py
+++ brexitTimer/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
+from django.http import HttpResponse
+from datetime import datetime, timedelta
+from django import template
+from django.utils.timesince import timeuntil
+
+
+brexitDate  = datetime(2019, 3, 29, 11, 00, 00, 0000)
+   
 # Create your views here.
+def index(request ,arg=None):
+     
+    return HttpResponse(timeuntil(brexitDate))
+
+
+
+   
